eed31b969bc4b9a9368f863f569b528f0cd562b5
gaillott <thomasgaillot@hotmail.com>
2026-09-04 11:10:14 +0200

    wording

--- app/page.tsx
+++ app/page.tsx
@@ -138,7 +138,7 @@ export default function HomePage() {
             <Link href="/contact">
               <div className="break-words gap-1 md:whitespace-nowrap flex-col sm:flex-row flex justify-center items-center">
                 <p>
-                  {"Samedi 12 septembre - Journée Portes Ouvertes - Entrée libre"}{" "}
+                  {"Samedi 12 Sept. - Journée Portes Ouvertes - Entrée libre"}{" "}
                 </p>
               </div>
             </Link>
